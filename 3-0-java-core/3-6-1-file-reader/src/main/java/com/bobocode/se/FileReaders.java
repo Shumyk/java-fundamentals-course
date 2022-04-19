c2f59dfc7e29676f448753b61507829d7f45d831
Shumyk <dima.shumyk@gmail.com>
2022-04-19 08:16:34 +0300

    3-6-1: file reader

--- 3-0-java-core/3-6-1-file-reader/src/main/java/com/bobocode/se/FileReaders.java
+++ 3-0-java-core/3-6-1-file-reader/src/main/java/com/bobocode/se/FileReaders.java
@@ -1,6 +1,14 @@
 package com.bobocode.se;
 
-import com.bobocode.util.ExerciseNotCompletedException;
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.stream.Stream;
+
+import static java.util.Objects.requireNonNull;
+import static java.util.stream.Collectors.joining;
 
 /**
  * {@link FileReaders} provides an API that allow to read whole file into a {@link String} by file name.
@@ -14,6 +22,33 @@ public class FileReaders {
      * @return string that holds whole file content
      */
     public static String readWholeFile(String fileName) {
-        throw new ExerciseNotCompletedException(); //todo
+        final var filePath = createPathFromFileName(fileName);
+        try (var fileLinesStream = openFileLinesStream(filePath)) {
+            return fileLinesStream.collect(joining("\n"));
+        }
+    }
+
+    private static Path createPathFromFileName(String fileName) {
+        requireNonNull(fileName);
+        final var resourceUrl = Thread.currentThread().getContextClassLoader().getResource(fileName);
+        try {
+            return Paths.get(resourceUrl.toURI());
+        } catch (NullPointerException | URISyntaxException e) {
+            throw new FileReaderException("Invalid file URL", e);
+        }
+    }
+
+    private static Stream<String> openFileLinesStream(Path filePath) {
+        try {
+            return Files.lines(filePath);
+        } catch (IOException e) {
+            throw new FileReaderException("Cannot create stream of file lines!", e);
+        }
+    }
+
+    public static class FileReaderException extends RuntimeException {
+        public FileReaderException(String msg, Exception e) {
+            super(msg, e);
+        }
     }
 }
